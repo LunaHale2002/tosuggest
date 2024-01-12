401f0ce5a72b27ba1e8a4b932728312286b83dd4
LunaHale2002 <lbabic@edenschool.fr>
2024-01-12 15:16:05 +0100

    tosuggest

---
+++ main.js
@@ -0,0 +1,39 @@
+const mots = [
+  "Variable",
+  "Fonction",
+  "Boucle",
+  "Tableau",
+  "Objet",
+  "Méthode",
+  "Événement",
+  "Condition",
+  "Opérateur",
+  "Chaîne",
+  "Nombre",
+  "Booléen",
+  "Undefined",
+  "Null",
+  "Callback",
+  "Promesse",
+  "Closure",
+  "Portée",
+  "DOM",
+  "AJAX",
+  "JSON",
+  "Node.js",
+  "Express",
+  "React",
+  "Vue",
+  "Angular",
+  "Module",
+  "ES6",
+  "Async/Await",
+  "Prototype"
+];
+
+const input = document.querySelector('input')
+input.addEventListener('input', (e)=>{
+  console.log(e);
+  // console.log("test");
+  // console.log(input.value);
+})
